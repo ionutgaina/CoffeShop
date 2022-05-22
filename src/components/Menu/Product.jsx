cc6a9187735688fa8535719e5efae98f4671c07e
IonutGaina <gainaionutz@gmail.com>
2022-05-23 02:38:23 +0300

    Menu completted

--- src/components/Menu/Product.jsx
+++ src/components/Menu/Product.jsx
@@ -2,20 +2,19 @@ import Swal from "sweetalert2";
 
 export const Product = (props) => {
   const ProductModal = (ProductInfo) => {
-    let rating = ProductInfo.rating.reduce((a, b) => a + b) / ProductInfo.rating.length;
-    rating = Math.round(rating * 10) / 10
+    let rating =
+      ProductInfo.rating.reduce((a, b) => a + b) / ProductInfo.rating.length;
+    rating = Math.round(rating * 10) / 10;
 
-    if ( rating === 0 )
-    rating = "not having "
-    else
-    rating = "having " + rating
+    rating === 0 ? (rating = "not having ") : (rating = "having " + rating);
+
     return Swal.fire({
       imageUrl: ProductInfo.image,
       imageWidth: 200,
       imageHeight: 200,
       title: ProductInfo.name + " " + ProductInfo.price + " LEI",
       text: ProductInfo.description,
-      footer: '<p>this product is ' + rating +' stars</p>',
+      footer: "<p>this product is " + rating + " stars</p>",
     });
   };
 
@@ -28,14 +27,14 @@ export const Product = (props) => {
       onClick={() => ProductModal(ProductInfo)}
     >
       <img
-        className="object-cover w-1/2 md:w-full md:h-60 mx-auto"
+        className="object-cover w-1/2 md:w-full h-16 md:h-60 mx-auto"
         src={ProductInfo.image}
         alt={"product " + ProductInfo.id}
       />
 
       <p
-        className="bg-primary text-black
-      font-bold w-1/2 md:w-full md:py-3 mb-1"
+        className="bg-primary text-black text-xs md:text-base
+      font-bold w-1/2 md:w-full m-auto md:py-3 mb-1"
       >
         {ProductInfo.name}
         {ProductInfo.quantity == 0 ? " is out of stock" : null}
